fe1431eecf915f1d02d328f531ebc90745e28f0e
lolovazquez <manuelvazjar@gmail.com>
2019-04-12 12:37:20 +0200

    service-offer

--- front/src/app/offers/offer.service.ts
+++ front/src/app/offers/offer.service.ts
@@ -1,9 +1,60 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'environments/environment';
+import { HandleError, HttpErrorHandler } from 'app/error.service';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Offer } from './offer';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OfferService {
 
-  constructor() { }
+  private entity_url = environment.REST_API_URL + 'offer';
+
+  private handlerError: HandleError;
+
+
+  constructor(private http: HttpClient, private httpErrorHandler: HttpErrorHandler) {
+    this.handlerError = httpErrorHandler.createHandleError('OfferService');
+  }
+
+  getOffers(): Observable<Offer[]> {
+    return this.http.get<Offer[]>(this.entity_url)
+      .pipe(
+        catchError(this.handlerError('getOffers', []))
+      );
+  }
+
+  getOfferById(offer_id: string): Observable<Offer> {
+    return this.http.get<Offer>(this.entity_url + '/' + offer_id)
+      .pipe(
+        catchError(this.handlerError('getOfferById', {} as Offer))
+      );
+  }
+
+  addOffer(offer: Offer): Observable<Offer> {
+    return this.http.post<Offer>(this.entity_url, offer)
+      .pipe(
+        catchError(this.handlerError('addOffer', offer))
+      );
+  }
+
+  updateOffer(offer_id: string, offer: Offer): Observable<Offer> {
+    return this.http.put<Offer>(this.entity_url + '/' + offer_id, offer)
+      .pipe(
+        catchError(this.handlerError('updateOffer', offer))
+      );
+  }
+
+  deleteOffer(offer_id: string): Observable<number> {
+    return this.http.delete<number>(this.entity_url + '/' + offer_id)
+      .pipe(
+        catchError(this.handlerError('deleteOffer', 0))
+      );
+
+  }
+
+
 }
