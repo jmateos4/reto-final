d6ee24e23016847f593bd2fcee5304e947d0b629
lolovazquez <manuelvazjar@gmail.com>
2019-04-12 13:51:21 +0200

    add offer

--- front/src/app/offers/offer-form/offer-form.component.ts
+++ front/src/app/offers/offer-form/offer-form.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Offer } from '../offer';
-import { HandleError } from 'app/error.service';
 import { OfferService } from '../offer.service';
 
 @Component({
@@ -10,22 +9,29 @@ import { OfferService } from '../offer.service';
 })
 export class OfferFormComponent implements OnInit {
 
-  offer : Offer;
-  private errorMessage: HandleError;
-  
-  constructor(private offerService : OfferService) { }
+  offer: Offer;
+  added_success: boolean = false;
+  errorMessage: string;
+  @Output() onNew = new EventEmitter<Offer>();
+
+  constructor(private offerService : OfferService) {
+    this.offer = <Offer>{};
+  }
 
   ngOnInit() {
   }
 
-  onSubmit(offer:Offer){
-  
+  onSubmit(offer: Offer) {
+    offer.id = null;
     this.offerService.addOffer(offer).subscribe(
       new_offer => {
         this.offer = new_offer;
+        this.added_success = true;
+        this.onNew.emit(this.offer) ;
       },
       error => this.errorMessage = <any>error
     );
-  }
+}
+
 
 }
