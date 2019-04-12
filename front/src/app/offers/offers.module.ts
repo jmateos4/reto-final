baa06d2929f558df06094a89daa6be0a3e7481df
DaniMSGit <danimorueta@gmail.com>
2019-04-12 12:25:13 +0200

    estructura2

--- front/src/app/offers/offers.module.ts
+++ front/src/app/offers/offers.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { OfferValidComponent } from './offer-valid/offer-valid.component';
+import { OfferFormComponent } from './offer-form/offer-form.component';
+import { OfferListComponent } from './offer-list/offer-list.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [OfferValidComponent, OfferFormComponent, OfferListComponent]
 })
 export class OffersModule { }
