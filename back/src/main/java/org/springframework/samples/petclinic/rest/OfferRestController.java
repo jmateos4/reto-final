f047fbfc0fcfb3361ad68b5c972f0df8a178f0ac
jmateos4 <javiermateos4@gmail.com>
2019-04-12 13:12:32 +0200

    @RestController añadido

--- back/src/main/java/org/springframework/samples/petclinic/rest/OfferRestController.java
+++ back/src/main/java/org/springframework/samples/petclinic/rest/OfferRestController.java
@@ -12,16 +12,16 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.samples.petclinic.model.Offer;
 import org.springframework.samples.petclinic.service.OfferService;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
-@Controller
+@RestController
 @CrossOrigin(exposedHeaders = "errors, content-type")
 @RequestMapping("/api/offers")
 public class OfferRestController {
