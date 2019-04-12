0f9572bbe400a218cc0e64cfddea1e1fc7afc02d
lolovazquez <manuelvazjar@gmail.com>
2019-04-12 13:44:07 +0200

    adfsaf

--- front/src/app/offers/offer-list/offer-list.component.ts
+++ front/src/app/offers/offer-list/offer-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Offer } from '../offer';
 import { Router } from '@angular/router';
+import { OfferService } from '../offer.service';
 
 @Component({
   selector: 'app-offer-list',
@@ -9,19 +10,41 @@ import { Router } from '@angular/router';
 })
 export class OfferListComponent implements OnInit {
 
-  offer: Offer[];
-
+  offers: Offer[];
+  errorMessage: string;
+  response_status: number;
   is_insert: boolean = false;
 
-
-  constructor(private router: Router) { }
-
-  ngOnInit() {
+  constructor(private offerService: OfferService, private router: Router) {
+    this.offers = [];
   }
 
- 
-  goFormOffer() {
-    this.router.navigate(['/offersAdd']);
+  ngOnInit() {
+    this.offerService.getOffers().subscribe(
+      offers => this.offers = offers,
+      error => this.errorMessage = <any> error);
+  }
+
+  deleteOffer(offer: Offer) {
+    this.offerService.deleteOffer(offer.id.toString()).subscribe(
+      response => {
+        this.response_status = response;
+        this.offers = this.offers.filter(current_item => !(current_item.id === offer.id));
+      },
+      error => this.errorMessage = <any> error);
+  }
+
+  onNewOffer(new_offer: Offer){
+    this.offers.push(new_offer);
+    this.showAddOfferComponent();
+  }
+
+  showAddOfferComponent() {
+    this.is_insert = !this.is_insert;
+  }
+
+  showEditOfferComponent(updated_offer: Offer) {
+    this.router.navigate(['/offers', updated_offer.id.toString(), 'edit']);
   }
 
   gotoHome() {
--- front/src/app/offers/offer.ts
+++ front/src/app/offers/offer.ts
@@ -1,4 +1,5 @@
 export interface Offer {
+    id: number;
     title:string;
     description:string;
     discount:number;
